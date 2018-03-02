fc28e824913f85945a32100ce0ad652a8c0a4f5e
gabrielx52 <gabriel.meringolo@gmail.com>
2018-03-02 14:25:16 -0800

    Added team logos. Added JQ to show team logo of selected team.

--- app/static/app.js
+++ app/static/app.js
@@ -3,3 +3,14 @@
 $(document).ready(function() {
     $(".dropdown-toggle").dropdown();
 });
+
+$(document).ready(function() {
+    $(".dropdown-item").click(function() {
+        let teamAbbr = $(this).data('team-abbr')
+        $("#team2-img").attr({
+            "style": "visibility: visible",
+            "src": `./../static/logos/${teamAbbr}_logo.svg`
+        })
+        $("#set-roster-r").removeAttr("disabled")
+    });
+});
