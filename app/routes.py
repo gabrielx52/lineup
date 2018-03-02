40909422383e9d13b11f046a8b9c0a9dd1fb13ab
gabrielx52 <gabriel.meringolo@gmail.com>
2018-03-02 11:56:44 -0800

    Starting frontend.

--- app/routes.py
+++ app/routes.py
@@ -3,9 +3,11 @@ from app import app
 
 from flask import render_template
 
+from .team_dict import TEAMS
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     """Index route."""
-    return render_template('index.html', title='Home Page')
+    return render_template('index.html', title='Home Page', teams=TEAMS)
