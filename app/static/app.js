74258095f7457c0e815a16987d00241020cfd8a3
gabrielx52 <gabriel.meringolo@gmail.com>
2018-03-02 20:01:28 -0800

    Added oppenents stats.

--- app/static/app.js
+++ app/static/app.js
@@ -39,47 +39,97 @@ $(document).ready(function() {
                 $("#left-tab-l").text(
                     `${netsStats['L']}`)
                 $("#left-tab-winpct").text(
-                    `${netsStats['W_PCT']}`)
+                    `${netsStats['W_PCT'].toFixed(3)}`)
                 $("#left-tab-min").text(
-                    `${netsStats['MIN']}`)
+                    `${netsStats['MIN'].toFixed(1)}`)
                 $("#left-tab-pts").text(
-                    `${netsStats['PTS']}`)
+                    `${netsStats['PTS'].toFixed(1)}`)
                 $("#left-tab-fgm").text(
-                    `${netsStats['FGM']}`)
+                    `${netsStats['FGM'].toFixed(1)}`)
                 $("#left-tab-fga").text(
-                    `${netsStats['FGA']}`)
+                    `${netsStats['FGA'].toFixed(1)}`)
                 $("#left-tab-fgpct").text(
-                    `${netsStats['FG_PCT']}`)
+                    `${(netsStats['FG_PCT'] * 100).toFixed(1)}`)
                 $("#left-tab-3pm").text(
-                    `${netsStats['FG3M']}`)
+                    `${netsStats['FG3M'].toFixed(1)}`)
                 $("#left-tab-3pa").text(
-                    `${netsStats['FG3A']}`)
+                    `${netsStats['FG3A'].toFixed(1)}`)
                 $("#left-tab-3ppct").text(
-                    `${netsStats['FG3_PCT']}`)
+                    `${(netsStats['FG3_PCT'] * 100).toFixed(1)}`)
                 $("#left-tab-ftm").text(
-                    `${netsStats['FTM']}`)
+                    `${netsStats['FTM'].toFixed(1)}`)
                 $("#left-tab-fta").text(
-                    `${netsStats['FTA']}`)
+                    `${netsStats['FTA'].toFixed(1)}`)
                 $("#left-tab-ftpct").text(
-                    `${netsStats['FT_PCT']}`)
+                    `${(netsStats['FT_PCT'] * 100).toFixed(1)}`)
                 $("#left-tab-oreb").text(
-                    `${netsStats['OREB']}`)
+                    `${netsStats['OREB'].toFixed(1)}`)
                 $("#left-tab-dreb").text(
-                    `${netsStats['DREB']}`)
+                    `${netsStats['DREB'].toFixed(1)}`)
                 $("#left-tab-reb").text(
-                    `${netsStats['REB']}`)
+                    `${netsStats['REB'].toFixed(1)}`)
                 $("#left-tab-ast").text(
-                    `${netsStats['AST']}`)
+                    `${netsStats['AST'].toFixed(1)}`)
                 $("#left-tab-tov").text(
-                    `${netsStats['TOV']}`)
+                    `${netsStats['TOV'].toFixed(1)}`)
                 $("#left-tab-stl").text(
-                    `${netsStats['STL']}`)
+                    `${netsStats['STL'].toFixed(1)}`)
                 $("#left-tab-blk").text(
-                    `${netsStats['BLK']}`)
+                    `${netsStats['BLK'].toFixed(1)}`)
                 $("#left-tab-blka").text(
-                    `${netsStats['BLKA']}`)
+                    `${netsStats['BLKA'].toFixed(1)}`)
                 $("#left-tab-pf").text(
-                    `${netsStats['PF']}`)
+                    `${netsStats['PF'].toFixed(1)}`)
+
+
+                $("#right-tab-gp").text(
+                    `${oppsStats['GP']}`)
+                $("#right-tab-w").text(
+                    `${oppsStats['W']}`)
+                $("#right-tab-l").text(
+                    `${oppsStats['L']}`)
+                $("#right-tab-winpct").text(
+                    `${oppsStats['W_PCT'].toFixed(3)}`)
+                $("#right-tab-min").text(
+                    `${oppsStats['MIN'].toFixed(1)}`)
+                $("#right-tab-pts").text(
+                    `${oppsStats['PTS'].toFixed(1)}`)
+                $("#right-tab-fgm").text(
+                    `${oppsStats['FGM'].toFixed(1)}`)
+                $("#right-tab-fga").text(
+                    `${oppsStats['FGA'].toFixed(1)}`)
+                $("#right-tab-fgpct").text(
+                    `${(oppsStats['FG_PCT'] * 100).toFixed(1)}`)
+                $("#right-tab-3pm").text(
+                    `${oppsStats['FG3M'].toFixed(1)}`)
+                $("#right-tab-3pa").text(
+                    `${oppsStats['FG3A'].toFixed(1)}`)
+                $("#right-tab-3ppct").text(
+                    `${(oppsStats['FG3_PCT'] * 100).toFixed(1)}`)
+                $("#right-tab-ftm").text(
+                    `${oppsStats['FTM'].toFixed(1)}`)
+                $("#right-tab-fta").text(
+                    `${oppsStats['FTA'].toFixed(1)}`)
+                $("#right-tab-ftpct").text(
+                    `${(oppsStats['FT_PCT'] * 100).toFixed(1)}`)
+                $("#right-tab-oreb").text(
+                    `${oppsStats['OREB'].toFixed(1)}`)
+                $("#right-tab-dreb").text(
+                    `${oppsStats['DREB'].toFixed(1)}`)
+                $("#right-tab-reb").text(
+                    `${oppsStats['REB'].toFixed(1)}`)
+                $("#right-tab-ast").text(
+                    `${oppsStats['AST'].toFixed(1)}`)
+                $("#right-tab-tov").text(
+                    `${oppsStats['TOV'].toFixed(1)}`)
+                $("#right-tab-stl").text(
+                    `${oppsStats['STL'].toFixed(1)}`)
+                $("#right-tab-blk").text(
+                    `${oppsStats['BLK'].toFixed(1)}`)
+                $("#right-tab-blka").text(
+                    `${oppsStats['BLKA'].toFixed(1)}`)
+                $("#right-tab-pf").text(
+                    `${oppsStats['PF'].toFixed(1)}`)
 
 
 
