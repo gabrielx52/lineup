8dc5bbe600be35a7a3affc26fff6b4bbc0f95ca7
gabrielx52 <gabriel.meringolo@gmail.com>
2018-03-09 19:38:12 -0800

    Added baseurl.

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,9 @@ from flask import Flask
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, support_credentials=True)
 
 from app import routes
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000, debug=True)
